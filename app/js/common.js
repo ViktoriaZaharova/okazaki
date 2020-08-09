d723cec0cc4df871347a677b419d09f889fbc13c
viktoriazaharova <zaharova181114@gmail.com>
2020-08-09 09:21:42 +0300

    media

--- app/js/common.js
+++ app/js/common.js
@@ -10,6 +10,101 @@ $('.catalog-item-dropdown').click(function (e) {
     $('.category-dropdown').slideToggle();
 });
 
+
+$('.radio input:radio').change(function(){
+    if($(this).is(":checked")) {
+        $(this).parents('.form-group__selection').find('.radio').removeClass('radio-click');
+        $(this).parents('.radio').addClass('radio-click');
+    } else {
+        $(this).parents('.radio').removeClass('radio-click');
+    }
+});
+
+$(".polzunok-1").slider({
+    min: -2000,
+    max: 1000,
+    values: [100, 700],
+    range: true,
+    animate: "fast",
+    slide : function(event, ui) {
+        $(".polzunok-input-left-1").val(ui.values[ 0 ]);
+        $(".polzunok-input-right-1").val(ui.values[ 1 ]);
+    }
+});
+$(".polzunok-input-left-1").val($(".polzunok-1").slider("values", 0));
+$(".polzunok-input-right-1").val($(".polzunok-1").slider("values", 1));
+$(document).focusout(function() {
+    var input_left = $(".polzunok-input-left-1").val().replace(/[^0-9]/g, ''),
+        opt_left = $(".polzunok-1").slider("option", "min"),
+        where_right = $(".polzunok-1").slider("values", 1),
+        input_right = $(".polzunok-input-right-1").val().replace(/[^0-9]/g, ''),
+        opt_right = $(".polzunok-1").slider("option", "max"),
+        where_left = $(".polzunok-1").slider("values", 0);
+    if (input_left > where_right) {
+        input_left = where_right;
+    }
+    if (input_left < opt_left) {
+        input_left = opt_left;
+    }
+    if (input_left == "") {
+        input_left = 0;
+    }
+    if (input_right < where_left) {
+        input_right = where_left;
+    }
+    if (input_right > opt_right) {
+        input_right = opt_right;
+    }
+    if (input_right == "") {
+        input_right = 0;
+    }
+    $(".polzunok-input-left-1").val(input_left);
+    $(".polzunok-input-right-1").val(input_right);
+    $(".polzunok-1").slider( "values", [ input_left, input_right ] );
+});
+
+$(".polzunok-2").slider({
+    min: 0,
+    max: 1000,
+    value: 700,
+    range: "min",
+    animate: "fast",
+    slide : function(event, ui) {
+        $(".polzunok-input-left-2").val(ui.value);
+    }
+});
+
+$(".polzunok-input-left-2").val($(".polzunok-2").slider("value"));
+
+
+$(".polzunok-3").slider({
+    min: 0,
+    max: 1000,
+    value: 700,
+    range: "min",
+    animate: "fast",
+    slide : function(event, ui) {
+        $(".polzunok-input-left-3").val(ui.value);
+    }
+});
+
+$(".polzunok-input-left-3").val($(".polzunok-3").slider("value"));
+
+$('.links-call').click(function (e) {
+    e.preventDefault();
+    $('.contact-popup').fadeToggle();
+});
+
+$('.contact-popup .modal__close').click(function () {
+    $('.contact-popup').fadeOut();
+});
+
+$('.btn-search').click(function () {
+    $(this).toggleClass('click')
+    $('.form-search').fadeToggle();
+});
+
+
 $(document).mouseup(function (e) { // событие клика по веб-документу
     var div = $(".category-dropdown"); // тут указываем ID элемента
     var btn = $('.catalog-item-dropdown');
@@ -31,6 +126,13 @@ $(document).ready(function () {
 
     open_modal.click(function (event) {
         event.preventDefault();
+
+        modal.animate({
+            opacity: 0,
+            top: '45%'
+        }).css('display', 'none');
+        overlay.fadeOut(400);
+
         var div = $(this).attr('href');
         overlay.fadeIn(400,
             function () {
